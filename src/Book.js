d6e97b2089a47283d8a7e3a94fbb1f0a414f2c5f
Flora René-Louis-Arthur <flora.rene@egmail.com>
2018-12-19 01:16:49 +0100

    subject: feat: First fully working version

---
+++ src/Book.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react'
+import * as BooksAPI from './BooksAPI'
+
+class Book extends Component {
+  state = {
+    newShelf: ''
+  }
+  getNewShelf = (e) => {
+    BooksAPI.update(this.props.book, e.target.value).then(() => {
+      this.props.updateBookList()
+    })
+    this.props.fish && this.props.fish()
+  }
+  getBooksShelf = () => {
+    const {library, book} = this.props
+    if (book.shelf !== undefined) {
+      return book.shelf
+    } else {
+      const match = library.filter((b) => {
+        return b.id === book.id
+      })
+      return match.length > 0
+        ? match[0].shelf
+        : 'none'
+    }
+  }
+  render() {
+    const {book, shelves, toTitlecase} = this.props
+    return (<li>
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{
+              width: 128,
+              height: 193,
+              backgroundImage: 'url("' + book.imageLinks.thumbnail + '")'
+            }}></div>
+          <div className="book-shelf-changer">
+            <select onChange={this.getNewShelf} value={this.getBooksShelf()}>
+              <option value="move" disabled="disabled">Move to...</option>
+              {shelves.map((s) => (<option value={s} key={s}>{toTitlecase(s)}</option>))}
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        {book.authors && (book.authors.map((a, index) => <div className="book-authors" key={a}>{a}<br/></div>))}
+      </div>
+    </li>)
+  }
+}
+
+export default Book
