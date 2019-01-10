1f415e72f91fa99d775a42406caa3c97e071ba39
Flora René-Louis-Arthur <flora.rene@egmail.com>
2019-01-10 18:23:19 +0100

    Updates

---
+++ precache-manifest.f505625a2487103a629b6dcb78b921fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c4c1eec6656f65c78d73",
+    "url": "/static/js/main.c4c1eec6.chunk.js"
+  },
+  {
+    "revision": "a16499fbe2b48c8d2e15",
+    "url": "/static/js/1.a16499fb.chunk.js"
+  },
+  {
+    "revision": "c4c1eec6656f65c78d73",
+    "url": "/static/css/main.d2b90c33.chunk.css"
+  },
+  {
+    "revision": "e533b1ff7b3c2055238deadaf1665e3a",
+    "url": "/index.html"
+  }
+];
